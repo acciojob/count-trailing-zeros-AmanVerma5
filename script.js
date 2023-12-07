b90a092fc5a17c78495b439fce81a1af6afc9529
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-12-07 15:23:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let fac=1;
+	for(let i=1;i<=n;i++){
+		fac=fac*i;
+	}
+	let count=0;
+	let flag=true;
+	while(flag){
+		if(fac%10===0){
+			count++;
+			fac=parseInt(fac/10);
+			continue;
+		}
+		flag=false;
+	}
+	return count;
 }
 
 const input = prompt("Enter a number");
